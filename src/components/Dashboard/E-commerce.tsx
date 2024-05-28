506bb8f8864b964828305c877076b3552de9a9e9
AaronCoorahua <ruben.coorahua@utec.edu.pe>
2024-05-28 15:26:17 -0500

    Actualización componentes

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -6,7 +6,7 @@ import ChartTwo from "../Charts/ChartTwo";
 import ChatCard from "../Chat/ChatCard";
 import TableOne from "../Tables/TableOne";
 import CardDataStats from "../CardDataStats";
-import MapOne from "../Maps/MapOne";
+
 
 const ECommerce: React.FC = () => {
   return (
@@ -101,11 +101,7 @@ const ECommerce: React.FC = () => {
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
-        <ChartThree />
-        <MapOne />
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
-        </div>
+        <ChartThree /> 
         <ChatCard />
       </div>
     </>
--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -44,16 +44,16 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            Thomas Anree
+            Aaron Coorahua
           </span>
-          <span className="block text-xs">UX Designer</span>
+          <span className="block text-xs">Sistemas</span>
         </span>
 
         <span className="h-12 w-12 rounded-full">
           <Image
             width={112}
             height={112}
-            src={"/images/user/user-01.png"}
+            src={"/images/user/user-aaron.png"}
             style={{
               width: "auto",
               height: "auto",
