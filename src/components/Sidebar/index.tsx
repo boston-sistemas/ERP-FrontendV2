1c600cbc3593f678fedcfed12551335cd03f5c4d
AaronCoorahua <ruben.coorahua@utec.edu.pe>
2024-05-28 16:07:59 -0500

    Actualizacion

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -286,7 +286,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                                 pathname === "/operaciones/revision-stock" && "text-white"
                               }`}
                             >
-                              Revisión Stock
+                              Revisión de Stock
                             </Link>
                           </li>
                           <li>
