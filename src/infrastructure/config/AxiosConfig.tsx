b13e6b2a118093fc926652ce5c1f7b6e64bb09a7
Angel Tito <91230666+Angel-Tito@users.noreply.github.com>
2025-03-17 09:53:25 -0500

    Cambio de puerto del server

--- src/infrastructure/config/AxiosConfig.tsx
+++ src/infrastructure/config/AxiosConfig.tsx
@@ -7,7 +7,7 @@ const server = 'http://10.0.1.13:8000/';
 const server2 = 'http://10.0.1.13:7000/';
 
 const instance = axios.create({
-  baseURL: server2,
+  baseURL: server,
   withCredentials: true,
 });
 
