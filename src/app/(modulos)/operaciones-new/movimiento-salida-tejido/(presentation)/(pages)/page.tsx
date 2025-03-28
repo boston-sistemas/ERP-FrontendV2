e83d6108f22c1913f26b0c44eb686f6d7eba7bcc
MaxAntunez404 <max.antunez@utec.edu.pe>
2025-03-27 20:04:20 -0500

    Salida tejido

---
+++ src/app/(modulos)/operaciones-new/movimiento-salida-tejido/(presentation)/(pages)/page.tsx
@@ -0,0 +1,22 @@
+﻿import Breadcrumb from "@/common/components/Breadcrumb/Breadcrumb";
+import MovIngresoHilado from "../components/Salidatejido";
+import DefaultLayout from "@/common/components/Layouts/DefaultLayout";
+import { Metadata } from "next"; 
+
+export const metadata: Metadata = {
+  title: "Movimiento Ingreso de Hilado",
+};
+
+
+const TablesPage = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="SALIDA DE TEJIDO" />
+      <div className="flex flex-col gap-10">
+        <MovIngresoHilado />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default TablesPage;
